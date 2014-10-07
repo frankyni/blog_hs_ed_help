db686889774630731de02f4eb116cc485c3e2794
frankyni <franky.chenxni@gmail.com>
2014-10-07 00:52:13 -0400

    added javascript and some css for buttons

---
+++ public/javascript/javascript.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	
+	$("#setting_btn1").click(function(){
+		$("#settings_form1").fadeToggle(0);
+	});
+
+	$("#setting_btn2").click(function(){
+		$("#settings_form2").fadeToggle(0);
+	});
+
+	$("#setting_btn3").click(function(){
+		$("#settings_form3").fadeToggle(0);
+	});
+
+	$("#setting_btn4").click(function(){
+		$("#settings_form4").fadeToggle(0);
+	});
+
+});
